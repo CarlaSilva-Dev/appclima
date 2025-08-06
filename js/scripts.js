3870384aea3e7cd9c77669b5b24f10c14d6b4905
Carla Silva <carlarayannedaslva@gmail.com>
2025-08-05 21:01:57 -0300

    novas alterações

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,58 @@
-//Variáveis
+// Variáveis
 const apiKey = "dc072d79ae3cef31b37c9f7436d64b88";
-const apiCountryURL = "https://countryflagsapi.com/png/";
+const apiCountryURL = "https://flagcdn.com/w320/";
 
 
-const cityInput = document.querySelector("city-input");
+const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
 
+const cityElement = document.querySelector('#city');
+const tempElement = document.querySelector('#temperature');
+const descElement = document.querySelector('#description'); // corrigido
+const weatherIconElement = document.querySelector('#weather-icon');
+const countryElement = document.querySelector('#country');
+const humidityElement = document.querySelector('#humidity span');
+const windElement = document.querySelector('#wind span');
 
-const cityElement = document.querySelector('#city')
-const tempElement = document.querySelector('#temperature')
-const descElement = document.querySelector('#descripition')
-const weatherIconElement = document.querySelector('#weather-icon')
-const countryElement = document.querySelector('#country')
-const umidityElement = document.querySelector('#umidity span')
-const windElement = document.querySelector('#wind span')
+const weatherContainer = document.querySelector("#weather-data");
 
-//Funções
-
-const getWeatherData = async(city) => {
-
-    const apiWeatherURL = `https://api.openweathermap.org/data/3.0/onecall?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
-
-
+// Funções
+const getWeatherData = async (city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
     const res = await fetch(apiWeatherURL);
     const data = await res.json();
-
-   return data;
+    return data;
 };
 
 const showWeatherData = async (city) => {
-    getWeatherData(city);
+    const data = await getWeatherData(city);
 
     cityElement.innerText = data.name;
-    tempElement.innerText = parseInt(data.main.temp)
-    descElement.innerText = dataweather[0]
-}
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    countryElement.setAttribute("src", `${apiCountryURL}${data.sys.country.toLowerCase()}.png`);
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed} km/h`;
 
+    weatherContainer.classList.remove("hide");
+};
 
-
-//Eventos
+// Eventos
 searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
+    const city = cityInput.value.trim();
+    if (city) {
+        showWeatherData(city);
+    }
+});
 
-    const city = cityInput.value;
 
+cityInput.addEventListener("keyup", (e) => {
 
-    showWeatherData("city");
+    if(e.code === "Enter") {
+        const city = e.target.value;
+
+        showWeatherData(city);
+    }
 })
